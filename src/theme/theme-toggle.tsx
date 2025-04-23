d9b3554a340b88ffc82f55efcb735ff69d1f5ec9
espinoza0 <gabriel.bascope005@gmail.com>
2025-04-23 12:26:51 +0200

    cambio en theme toggle, error

--- src/theme/theme-toggle.tsx
+++ src/theme/theme-toggle.tsx
@@ -2,10 +2,18 @@
 
 import { useTheme } from "next-themes";
 import { Sun, Moon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return;
+  
   return (
     <button
       type="button"
